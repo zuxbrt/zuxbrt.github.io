13ae5a02f4b49906d20921c7b0df6a7f7838aa61
zuxbrt <zmuhovic97@gmail.com>
2025-05-02 17:48:10 +0200

    remove basePath and assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  basePath: '/',
-  assetPrefix: '/',
+  // basePath: '/',
+  // assetPrefix: '/',
   reactStrictMode: true
 };
 
